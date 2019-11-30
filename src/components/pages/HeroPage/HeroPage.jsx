4ca51a4c3b2eb2c4474e96a0242006351319ba53
Sean Lin <seanlin05583@gmail.com>
2019-11-30 11:37:30 +0800

    Add circle spinner when loading

--- src/components/pages/HeroPage/HeroPage.jsx
+++ src/components/pages/HeroPage/HeroPage.jsx
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Route } from "react-router-dom";
 import HeroList from './HeroList';
 import HeroProfile from './HeroProfile';
+import { CircleSpinner } from 'components/common';
 import { apiGetHeroList } from 'api';
+import classNames from 'classnames/bind';
+import style from './style.css';
+
+const cx = classNames.bind(style);
 
 const HeroPage = () => {
   const [heroList, setHeroList] = useState([]);
@@ -20,9 +25,13 @@ const HeroPage = () => {
 
   return (
     <>
-      {isFetchingHeroList && <div>spinner</div>}
+      {isFetchingHeroList && (
+        <div className={cx('circle-spinner-container')}>
+          <CircleSpinner />
+        </div>
+      )}
       {!isFetchingHeroList && <>
-        <HeroList heroList={heroList}/>
+        <HeroList heroList={heroList} />
         <Route path="/heroes/:heroId">
           <HeroProfile />
         </Route>
